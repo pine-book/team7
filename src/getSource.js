5c6f4bef5be466ecff1e101bbe552690aa580957
Tatsumi <tatsumi719@gmail.com>
2021-07-24 01:08:32 +0900

    modify getSourse.js

--- src/getSource.js
+++ src/getSource.js
@@ -1,29 +1,32 @@
 let rq;
 var frag;
-let month = 7, day = 23;
- function read() {
-     rq = new XMLHttpRequest();
-     rq.open("GET", "Sample.json");
-     rq.onreadystatechange = receive;
-     rq.send(null);
-  }
+let month = 7,
+    day = 23;
 
-  function receive() {
-     if (rq.readyState == 4) {
-        
+function read() {
+    rq = new XMLHttpRequest();
+    rq.open("GET", "Sample.json");
+    rq.onreadystatechange = receive;
+    rq.send(null);
+}
+
+function receive() {
+    let tmp;
+    if (rq.readyState == 4) {
+
         if (rq.status == 200) {
-           jdat = rq.responseText;
-           let dat = eval("(" + jdat + ")");
+            jdat = rq.responseText;
+            let dat = eval("(" + jdat + ")");
 
-           const elm = document.getElementById("day_task");
-           
-           for (let i = 0; i < dat.length; i++) {
-              if (month == dat[i].month && day == dat[i].day && dat[i].day != frag) {
-                  let str = "<p>" + dat[i].time + "  " + dat[i].name +"</p>";
-                  elm.innerHTML += str;
-              }
-           }
-           frag = dat[i].day
+            const elm = document.getElementById("day_task");
+            if (0 == elm.children.length) {
+                for (let i = 0; i < dat.length; i++) {
+                    if (month == dat[i].month && day == dat[i].day) {
+                        let str = '<p class="task_time">' + dat[i].time + '</p>' + '<p class="task_name">' + dat[i].name + '</p>';
+                        elm.innerHTML += str;
+                    }
+                }
+            }
         }
-     }
- }
+    }
+}
