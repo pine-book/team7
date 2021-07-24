7f26271ec29cb4582b44bee1d5637ce53c31ae83
Tatsumi <tatsumi719@gmail.com>
2021-07-24 19:10:40 +0900

    create calendar.js

--- src/schedule.js
+++ src/schedule.js
@@ -1,11 +1,13 @@
 function show_day() {
     document.getElementById("date").innerHTML = get_day();
- }
- function get_day() {
+    create_cal()
+}
+
+function get_day() {
     var now = new Date();
     var year = now.getFullYear();
     var mon = now.getMonth() + 1; //１を足すこと
     var day = now.getDate();
     var s = year + "/" + mon + "/" + day;
     return s;
- }
+}
